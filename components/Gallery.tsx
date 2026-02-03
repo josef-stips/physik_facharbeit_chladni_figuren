f8fa4642106b0095cb20ea84b0a479f6c5ee14ba
JosefStips <stipsjosef@gmail.com>
2026-02-03 20:38:57 +0100

    add all frequencies associated with the chladni figure photos

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,22 +1,50 @@
-"use client"
+'use client';
 
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import testedFrequencies from '@/types/testesFrequencies';
+
+interface PhotoMetaData {
+	fileName: string;
+	frequency: number | '???';
+	isNote: boolean;
+	note?: string;
+	noteTuning?: string;
+	annotation?: string;
+}
 
 export default function Gallery() {
-  const [photos, setPhotos] = useState<string[]>([]);
+	const [photos, setPhotos] = useState<string[]>([]);
 
-  useEffect(() => {
-    fetch("/api/photos")
-      .then(res => res.json())
-      .then(data => setPhotos(data));
-  }, []);
+	useEffect(() => {
+		fetch('/api/photos')
+			.then((res) => res.json())
+			.then((data) => setPhotos(data));
+	}, []);
 
-  return (
-    <div className="grid grid-cols-3 gap-4">
-      {photos.map((url) => (
-        <Image width={200} height={200} key={url} src={url} alt="Photo" className="w-full h-auto object-cover" />
-      ))}
-    </div>
-  );
+	const photoMetaData: PhotoMetaData[] = photos.map((ph, i) => ({
+		fileName: ph.slice(87, 100),
+		frequency: testedFrequencies[i]?.f,
+		isNote: !!testedFrequencies[i]?.n,
+		annotation: testedFrequencies[i]?.a,
+		note: testedFrequencies[i]?.n,
+		noteTuning: testedFrequencies[i]?.t,
+	}));
+
+	return (
+		<div className="grid grid-cols-3 gap-4">
+			{photos.map((url, i) => (
+				<div key={url}>
+					<Image
+						width={200}
+						height={200}
+						src={url}
+						alt="Photo"
+						className="w-full h-auto object-cover"
+					/>
+					<p>{photoMetaData[i]?.frequency}</p>
+				</div>
+			))}
+		</div>
+	);
 }
